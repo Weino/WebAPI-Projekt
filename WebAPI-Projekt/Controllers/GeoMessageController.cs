9d8a5cfd84f7694b4e3b550571bcf9495ead281b
william weinö <william.weino@hotmail.com>
2022-02-08 14:27:03 +0100

    adding standard DBcontext

--- WebAPI-Projekt/Controllers/GeoMessageController.cs
+++ WebAPI-Projekt/Controllers/GeoMessageController.cs
@@ -1,6 +1,20 @@
-﻿namespace WebAPI_Projekt.Controllers
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI_Projekt.Models;
+using RouteAttribute = Microsoft.AspNetCore.Mvc.RouteAttribute;
+
+namespace WebAPI_Projekt.Controllers
 {
-    public class GeoMessageController
+
+    [Route("api/GeoComments")]
+    [ApiController]
+    public class GeoMessageController : ControllerBase
     {
+        [HttpGet("Id")]
+        public async Task<ActionResult<GeoMessage>> GetGeoComment(int id)
+        {
+            //test var message = new GeoMessage();
+                return Ok();
+        }
     }
 }
--- WebAPI-Projekt/Data/GeoMessageDbContext.cs
+++ WebAPI-Projekt/Data/GeoMessageDbContext.cs
@@ -1,6 +1,19 @@
-﻿namespace WebAPI_Projekt.Data
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPI_Projekt.Models;
+
+namespace WebAPI_Projekt.Data
 {
-    public class GeoMessageDbContext
+    public class GeoMessageDbContext : DbContext
     {
+
+        public GeoMessageDbContext(DbContextOptions<GeoMessageDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<GeoMessage> GeoMessages { get; set; }
+
     }
+
 }
+ 
